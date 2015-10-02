c616adcd18a9171ee8b451a00849d9eeb799a59b
John <jjpizzo4@gmail.com>
2015-10-02 18:28:07 -0400

    text

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-s = require('express');
+express = require('express');
 var app = express();
-var port = 3002;
+var port = 8000;
 app.use(express.static(__dirname));
 console.log("Server listening on port", port + "...");
 app.listen(port);
