3e0938ca54fe4d29588038390318632c17cafeb2
John <jjpizzo4@gmail.com>
2015-10-07 22:59:32 -0400

    server additions

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 express = require('express');
+path = require('path');
+
 var app = express();
 var port = 8000;
-app.use(express.static(__dirname));
+
+app.use(express.static(path.resolve(__dirname, '../frontend')));
 console.log("Server listening on port", port + "...");
 app.listen(port);
