4b78e9f5571617801ab7d3966fbb8b6b980a3640
John Pizzo <johnpizzo@Johns-MacBook-Pro.local>
2015-10-07 23:00:37 -0400

    Youtube videos by link and style

--- api/index.js
+++ api/index.js
@@ -2,5 +2,8 @@ express = require('express');
 var app = express();
 var port = 8000;
 app.use(express.static(__dirname));
+app.use(function() {
+	console.log('Request...');
+});
 console.log("Server listening on port", port + "...");
 app.listen(port);
